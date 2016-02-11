647bdbcf39c48d6faba72d049c5991a1f588ae5c
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2016-02-11 02:24:29 -0200

    Subset stock by reports added. Documentation fixed.

--- R/netvl_data.R
+++ R/netvl_data.R
@@ -52,3 +52,111 @@ netvl.to.ts <- function(data, col.name, interval = "quarter") {
     
     return(ts)
 }
+
+.netvl.subset.stock <- function(report.df, stock.df,
+                                by.mean = FALSE, include.first = TRUE) {
+    if (by.mean == FALSE & include.first == FALSE) {
+        stop("Specify method.")
+    }
+    else if (by.mean == FALSE & include.first == TRUE) {
+        subset.stock <- NULL
+        
+        for (i in 1:nrow(report.df)) {
+            date.i <- report.df[i, "date"]
+            s <- NULL
+            date.range <- stock.df$date >= date.i
+            s <- stock.df[date.range, ][1, ]
+            if (!any(date.range))  { break }
+            if (!is.null(s)) {
+                subset.stock <- rbind(subset.stock, s)
+            }
+        }
+        
+        rownames(subset.stock) <- 1:nrow(subset.stock)
+    }
+    else if(by.mean == TRUE & include.first == FALSE) {
+        subset.stock <- NULL
+        dates <- numeric(0)
+        class(dates) <- "Date"
+        i <- 1
+        
+        for (i in 1:nrow(report.df)) {
+            date.prev <- report.df[ifelse(i == 1, 1, (i-1)), "date"]
+            date.i <- report.df[i, "date"]
+            s <- NULL
+            
+            if (i == 1) {
+                date.range <- (stock.df$date <= date.i)
+                if (!any(date.range)) { break }
+                s <- stock.df[date.range, ]
+            }
+            else {
+                date.range <- (stock.df$date >= date.prev & stock.df$date <= date.i)
+                if (!any(date.range)) { break }
+                s <- stock.df[date.range, ]
+            }
+            
+            date.last <- s[nrow(s), "date"]
+            dates <- c(dates, date.last)
+            s <- s[, 2:ncol(s)]
+            
+            if (!is.null(s)) {
+                s <- as.vector(apply(as.matrix(s), 2, mean, na.rm = T))
+                subset.stock <- rbind(subset.stock, s)
+            }
+        }
+        
+        subset.stock <- data.frame(matrix(subset.stock, nrow = nrow(subset.stock)))
+        subset.stock <- cbind(dates, subset.stock)
+        
+        names(subset.stock) <- names(stock.df)
+    }
+    else if(by.mean == TRUE & include.first == TRUE) {
+        subset.stock <- NULL
+        dates <- numeric(0)
+        class(dates) <- "Date"
+        i <- 1
+        
+        for (i in 1:nrow(report.df)) {
+            date.prev <- report.df[ifelse(i == 1, 1, (i-1)), "date"]
+            date.i <- report.df[i, "date"]
+            s <- NULL
+            
+            if (i == 1) {
+                date.range <- (stock.df$date <= date.i)
+                if (!any(date.range)) { break }
+                s <- stock.df[date.range, ]
+                date.next <- (stock.df$date >= date.i)
+                if (any(date.next)) {
+                    s <- rbind(s, stock.df[date.next, ][1, ])
+                }
+            }
+            else {
+                date.range <- (stock.df$date >= date.prev & stock.df$date <= date.i)
+                if (!any(date.range)) { break }
+                date.range[min(which(date.range == TRUE))] <- FALSE
+                s <- stock.df[date.range, ]
+                date.next <- (stock.df$date >= date.i)
+                if (any(date.next)) {
+                    s <- rbind(s, stock.df[date.next, ][1, ])
+                }
+            }
+            
+            date.last <- s[nrow(s), "date"]
+            dates <- c(dates, date.last)
+            s <- s[, 2:ncol(s)]
+            
+            if (!is.null(s)) {
+                s <- as.vector(apply(as.matrix(s), 2, mean, na.rm = T))
+                subset.stock <- rbind(subset.stock, s)
+            }
+        }
+        
+        subset.stock <- data.frame(matrix(subset.stock, nrow = nrow(subset.stock)))
+        subset.stock <- cbind(dates, subset.stock)
+        
+        names(subset.stock) <- names(stock.df)
+    }
+    
+    return(subset.stock)
+}
